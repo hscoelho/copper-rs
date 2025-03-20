fe3b10f64eadeaa4486b6c9545977eb89e4ae3a2
hscoelho <henriquescharlaucoelho@gmail.com>
2025-03-20 14:51:51 -0300

    simplified example by removing int source task
    this is probably the minimal example that exemplifies the panic

--- examples/cu_panic/src/tasks.rs
+++ examples/cu_panic/src/tasks.rs
@@ -1,79 +1,40 @@
 use cu29::prelude::*;
-use std::{fmt::Display, marker::PhantomData};
 
 #[derive(Default)]
-pub struct StrSrcTask {
-    number_of_executions: i64,
+pub struct SrcTask {
+    first_execution: bool,
 }
 
-impl Freezable for StrSrcTask {}
+impl Freezable for SrcTask {}
 
-impl<'cl> CuSrcTask<'cl> for StrSrcTask {
+impl<'cl> CuSrcTask<'cl> for SrcTask {
     type Output = output_msg!('cl, String);
     fn new(_config: Option<&ComponentConfig>) -> CuResult<Self> {
         Ok(Self {
-            number_of_executions: 0,
+            first_execution: true,
         })
     }
 
     fn process(&mut self, _clock: &RobotClock, output: Self::Output) -> CuResult<()> {
-        if self.number_of_executions == 0 {
+        if self.first_execution {
+            // uncommenting this line solves the panic
             // output.clear_payload();
-        } else if self.number_of_executions == 1 {
-            output.set_payload("SrcTask ran one time".into());
         } else {
-            output.clear_payload();
+            output.set_payload("SrcTask is running".into());
         }
-        self.number_of_executions += 1;
+        self.first_execution = false;
 
         Ok(())
     }
 }
 
 #[derive(Default)]
-pub struct IntSrcTask {
-    number_of_executions: i64,
-}
+pub struct SinkTask {}
 
-impl Freezable for IntSrcTask {}
+impl Freezable for SinkTask {}
 
-impl<'cl> CuSrcTask<'cl> for IntSrcTask {
-    type Output = output_msg!('cl, i64);
-    fn new(_config: Option<&ComponentConfig>) -> CuResult<Self> {
-        Ok(Self {
-            number_of_executions: 0,
-        })
-    }
-
-    fn process(&mut self, _clock: &RobotClock, output: Self::Output) -> CuResult<()> {
-        if self.number_of_executions == 0 {
-            // output.clear_payload();
-        } else if self.number_of_executions == 1 {
-            output.set_payload(self.number_of_executions);
-        } else {
-            output.clear_payload();
-        }
-        self.number_of_executions += 1;
-
-        Ok(())
-    }
-}
-
-pub type StrSinkTask = SinkTask<String>;
-pub type IntSinkTask = SinkTask<i64>;
-
-#[derive(Default)]
-pub struct SinkTask<T> {
-    _marker: PhantomData<T>,
-}
-
-impl<T> Freezable for SinkTask<T> {}
-
-impl<'cl, T> CuSinkTask<'cl> for SinkTask<T>
-where
-    T: CuMsgPayload + 'cl + 'static + Display,
-{
-    type Input = input_msg!('cl, T);
+impl<'cl> CuSinkTask<'cl> for SinkTask {
+    type Input = input_msg!('cl, String);
 
     fn new(_config: Option<&ComponentConfig>) -> CuResult<Self> {
         Ok(Self::default())
